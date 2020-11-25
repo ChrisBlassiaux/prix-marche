5fe67eded61e432428855f58b53e68aadac6b91e
ChrisBlassiaux <chrisblassiaux@gmail.com>
2020-11-25 17:20:17 +0100

    feat(seed): init seeder

---
+++ database/factories/CategoryFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Category;
+use Faker\Generator as Faker;
+
+$factory->define(Category::class, function (Faker $faker) {
+    return [
+        'name' => $faker->word,
+        'icon' => '<i class="fa fa-facebook fa-3x"></i>'    ];
+});
