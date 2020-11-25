c5feebd48d8ba6f7acdd2b378264619a17c40e4e
ChrisBlassiaux <chrisblassiaux@gmail.com>
2020-11-25 17:30:57 +0100

    feat(seed): fix seed category

--- database/seeds/CategoriesTableSeeder.php
+++ database/seeds/CategoriesTableSeeder.php
@@ -11,6 +11,6 @@ class CategoriesTableSeeder extends Seeder
      */
     public function run()
     {
-        factory('App\Product', 15)->create();
+        factory('App\Category', 15)->create();
     }
 }
